cbe868f4aa25deff907aa5fc8d5b44828f3bc095
JaumeAL <jsalas2@nigul.cide.es>
2025-03-07 20:44:51 +0100

    colocar botones

--- src/com/shridhar/views/GameScreen1.java
+++ src/com/shridhar/views/GameScreen1.java
@@ -6,19 +6,18 @@ import javax.swing.border.EmptyBorder;
 import com.shridhar.personalGame.GameFrame1;
 import java.awt.event.*;
 
-
 public class GameScreen1 extends JFrame {
 
 	private JPanel contentPane;
 
 	Timer timer;
 	int counter = 1;
-	
+
 	private void animation() {
-			timer = new Timer(10, (e)->{
+		timer = new Timer(10, (e) -> {
 			progressBar.setValue(counter);
 			counter++;
-			if(counter>=100) {
+			if (counter >= 100) {
 				this.setVisible(false);
 				this.dispose();
 				timer.stop();
@@ -28,7 +27,7 @@ public class GameScreen1 extends JFrame {
 		});
 		timer.start();
 	}
-	
+
 	private void help() {
 		this.setVisible(false);
 		this.dispose();
@@ -36,15 +35,15 @@ public class GameScreen1 extends JFrame {
 		help.setVisible(true);
 		help.setLocationRelativeTo(null);
 	}
-	
+
 	private void customizePlayer() {
-        this.setVisible(false);
-        this.dispose();
-        NameScreen customizeScreen = new NameScreen();
-        customizeScreen.setVisible(true);
-        customizeScreen.setLocationRelativeTo(null);
-    }
-	
+		this.setVisible(false);
+		this.dispose();
+		NameScreen customizeScreen = new NameScreen();
+		customizeScreen.setVisible(true);
+		customizeScreen.setLocationRelativeTo(null);
+	}
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -60,32 +59,33 @@ public class GameScreen1 extends JFrame {
 	}
 
 	JProgressBar progressBar = new JProgressBar();
-	public GameScreen1() {//PANTALLA INICIAL
+
+	public GameScreen1() {// PANTALLA INICIAL
 		setTitle("CIDE FIGHTER");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 370, 500);
-		
+
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(232, 232, 232));//COLOR DE FONDO
+		contentPane.setBackground(new Color(232, 232, 232));// COLOR DE FONDO
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		//IMAGEN
-		ImageIcon icono = new ImageIcon(getClass().getResource("/com/shridhar/personalGame/cide.png")); //Ruta de la imagen
-        JLabel etiqueta = new JLabel(icono);
-		etiqueta.setBounds(80, 5, 190, 190); //UBICACIÓN DE LA IMAGEN
+		// IMAGEN
+		ImageIcon icono = new ImageIcon(getClass().getResource("/com/shridhar/personalGame/cide.png")); // Ruta de la
+																										// imagen
+		JLabel etiqueta = new JLabel(icono);
+		etiqueta.setBounds(80, 5, 190, 190); // UBICACIÓN DE LA IMAGEN
 		contentPane.add(etiqueta);
-		
-		//BARRA DE CARGA
-		progressBar.setBackground(new Color(190, 190, 190));//COLOR DE FONDO
+
+		// BARRA DE CARGA
+		progressBar.setBackground(new Color(190, 190, 190));// COLOR DE FONDO
 		progressBar.setStringPainted(true);
-		progressBar.setBounds(35, 200, 278, 28);//UBICACIÓN
+		progressBar.setBounds(35, 200, 278, 28);// UBICACIÓN
 		progressBar.setVisible(false);
 		contentPane.add(progressBar);
-		
-		
-		//BOTÓN START
+
+		// BOTÓN START
 		JButton btnStartGame = new JButton("JUGAR");
 		btnStartGame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -93,43 +93,44 @@ public class GameScreen1 extends JFrame {
 				progressBar.setVisible(true);
 			}
 		});
-		btnStartGame.setBackground(new Color(190, 190, 190));//COLOR FONDO BOTÓN
-		btnStartGame.setForeground(new Color(235, 235, 235));//COLOR DE FONDO
-		btnStartGame.setBackground(new Color(44, 150, 57));//COLOR LETRA
+		btnStartGame.setBackground(new Color(190, 190, 190));// COLOR FONDO BOTÓN
+		btnStartGame.setForeground(new Color(235, 235, 235));// COLOR DE FONDO
+		btnStartGame.setBackground(new Color(44, 150, 57));// COLOR LETRA
 		btnStartGame.setFont(new Font("Tahoma", Font.BOLD, 28));
-		btnStartGame.setBounds(53, 237, 241, 59);//UBICACIÓN DEL BOTÓN
+		btnStartGame.setBounds(53, 237, 241, 55);// UBICACIÓN DEL BOTÓN
 		contentPane.add(btnStartGame);
-		
-		//BOTÓN MI JUGADOR
-		JButton btnCustomize = new JButton("JUGADOR");
-        btnCustomize.addActionListener(e -> customizePlayer());
-        btnCustomize.setForeground(new Color(235, 235, 235));
-        btnCustomize.setBackground(new Color(44, 150, 57));
-        btnCustomize.setFont(new Font("Tahoma", Font.BOLD, 28));
-        btnCustomize.setBounds(53, 318, 241, 50);//UBICACIÓN
-        contentPane.add(btnCustomize);
 
-		//BOTÓN HELP
+		// BOTÓN MI JUGADOR
+		JButton btnCustomize = new JButton("JUGADOR");
+		btnCustomize.addActionListener(e -> customizePlayer());
+		btnCustomize.setForeground(new Color(235, 235, 235));
+		btnCustomize.setBackground(new Color(44, 150, 57));
+		btnCustomize.setFont(new Font("Tahoma", Font.BOLD, 28));
+		btnCustomize.setBounds(53, 313, 241, 55);// UBICACIÓN
+		contentPane.add(btnCustomize);
+
+		// BOTÓN HELP
 		JButton btnNewButton = new JButton("AJUDA");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				help();
 			}
 		});
-		btnNewButton.setBackground(new Color(190, 190, 190));//COLOR FONDO BOTÓN
+		btnNewButton.setBackground(new Color(190, 190, 190));// COLOR FONDO BOTÓN
 		btnNewButton.setForeground(new Color(235, 235, 235));
 		btnNewButton.setBackground(new Color(44, 150, 57));
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 28));
-		btnNewButton.setBounds(80, 390, 181, 52);//UBICACIÓN DEL BOTÓN
+		btnNewButton.setBounds(53, 390, 241, 55);// UBICACIÓN DEL BOTÓN
 		contentPane.add(btnNewButton);
 		try {
-			Font font = Font.createFont(Font.TRUETYPE_FONT, getClass().getResourceAsStream("/com/shridhar/personalGame/junegull.ttf"));
+			Font font = Font.createFont(Font.TRUETYPE_FONT,
+					getClass().getResourceAsStream("/com/shridhar/personalGame/junegull.ttf"));
 			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 			ge.registerFont(font);
 			Font buttonFont = font.deriveFont(Font.BOLD, 28f);
-			
+
 			// Set the font for the buttons
-			btnStartGame.setFont(buttonFont); 
+			btnStartGame.setFont(buttonFont);
 			btnCustomize.setFont(buttonFont);
 			btnNewButton.setFont(buttonFont);
 		} catch (Exception e) {
